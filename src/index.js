e64fd36880da61338894daf1844d1f11aa83b3f1
desen <32864798+desen94@users.noreply.github.com>
2021-08-28 22:20:48 +0500

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 function eval() {
     // Do not use eval!!!
-    return;
+    return
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    expr = expr.replace(/ /g, '')
+    
+    let aloneBrackets = 0
+    expr.split('').forEach((char) => {
+        if (char === '(') aloneBrackets++
+        if (char === ')') aloneBrackets--
+    })
+    
+    if (aloneBrackets) throw Error('ExpressionError: Brackets must be paired')
+    
+    if (expr.includes('/0')) throw Error('TypeError: Division by zero.')
+    
+    expr = new Function(`return ${expr}`)
+    
+    return expr()
 }
 
 module.exports = {
